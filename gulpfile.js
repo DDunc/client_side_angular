28fbb25124338bf2d928239dd95661d475028bd1
Duncan <Duncan.David@gmail.com>
2015-09-29 11:41:34 -0700

    remove gulp-watch module

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-//var files = ['*/**.js', '!./node_modules/**'];
 var webpack = require('webpack-stream');
-var watch = require('gulp-watch');
 
-//pipe webpack
-//all dependencies for client and server are in one file, is the plan
 gulp.task('webpack:dev', function() {
   return gulp.src('./app/js/client.js')
   .pipe(webpack({
